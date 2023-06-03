59529272739a84391ac098b264e88244ceedc9b7
George Lemon <georgelemon@protonmail.com>
2023-06-03 23:27:23 +0300

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/commands/runCommand.nim
+++ src/commands/runCommand.nim
@@ -1,22 +1,25 @@
+import emitter
 import kapsis/[runtime, cli]
-import std/[strutils, os, osproc]
+import std/[strutils, os, osproc, typeinfo]
 import ../madam/[serve, configurator]
 
 proc runCommand*(v: Values) =
-  let Config = Configurator.init()
-  let port = Config.instance.getPort()
-  if Config.status == true:
+  let config = newConfig()
+  let port = config.instance.getPort()
+  if config.status == true:
+    Event.init()
+    include ../madam/listeners
     display("------------------------------------------------", indent=2, br="before")
-    display("💃 Madam is dancing on http://$1:$2" % ["localhost", $port], indent=4)
+    display("💃 Madam is dancing http://$1:$2" % ["localhost", $port], indent=4)
     display("------------------------------------------------", indent=2, br="after")
     # display("Registered routes:", indent=2)
     # TODO list all registered routes as [VERB]  route  entry.html
     sleep(200)
-    discard execCmd("open http://localhost:" & $port)
+    if config.instance.useBrowser():
+      discard execCmd("open http://localhost:" & $port)
     try:
-      startHttpServer(Config.instance)
+      startHttpServer(config.instance)
     except OSError:
       echo getCurrentExceptionMsg()
-
-  else:
-    display("👉 Could not start a server on $1:$2" % ["localhost", $port], indent=4)
+  # else:
+    # display("👉 Could not start a server on $1:$2" % ["localhost", $port], indent=4)
